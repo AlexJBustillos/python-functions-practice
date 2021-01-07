7d50e82ed3fde6a9f80e94896e287ff274d32d20
alexjbustillos <alexjbustillos@gmail.com>
2021-01-07 16:12:43 -0500

    [fizzbuzz]finish

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+n = int(input("enter a number please: "))
+def fizzbuzz(n):
+    n = list(range(n + 1))
+    for i in range(len(n)):
+        if i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        elif i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        else:
+            print(i)
+            
+fizzbuzz(n)
