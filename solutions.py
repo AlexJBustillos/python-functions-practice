f60e1441c4a12c8662a0cb80f5db3b751c7e2e75
alexjbustillos <alexjbustillos@gmail.com>
2021-01-08 10:12:22 -0500

    [update]

--- solutions.py
+++ solutions.py
@@ -12,13 +12,13 @@ print(tripler([1,5, 7, 8, 89]))
 
 def odd_range(end):
     arr = []
-    for i in range(1, end+1, 2):
+    for i in range(1, end, 2):
         arr.append(i)
     return arr
 
 
 
-print(odd_range(13))
+print(odd_range(32))
 
 def cat_builer(name, color, toys):
     return {
